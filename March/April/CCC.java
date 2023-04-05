e3d167046081d112a0c6d41dc466b652fec2cabf
SL6I <s443004498@st.uqu.edu.sa>
2023-04-05 17:10:45 +0300

    D

---
+++ March/April/CCC.java
@@ -0,0 +1,34 @@
+package April;
+
+public class CCC {
+
+    // private nfs alklas bs
+    // 5.8
+    // set and get 
+    // 5.13,5.14
+    //5.15 , 5.16
+    /**
+     javadoc
+
+     */
+    // overloading con
+    // sgnuture
+    public static void main(String[] args) {
+        
+    }
+    public static int c(int n1,double n2){
+            return(int)( n1 + n2) ;
+    }
+    //6.1
+    //char and int not overloading
+    //6.16
+    // 6.18 , 6.19
+    // array object :( 7.18 , 7.19 page 555 and 7.13 diffrent object
+    // just public inheretince not private u cannot acces private 8.2-3
+    // final modifer cannot overridieng 7ta alclass mmkn ykon final wma t8dr tswe wratha
+    // super super super
+    // 8.3 mhm
+    // class object mhm 8.8 kanh ab
+
+
+}
