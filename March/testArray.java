3a7cdba60155cc7b2e10c2afb4dfd456fb4d42ab
SL6I <s443004498@st.uqu.edu.sa>
2023-04-17 22:57:56 +0300

    array

---
+++ March/testArray.java
@@ -0,0 +1,14 @@
+/**
+ * testArray
+ */
+public class testArray {
+
+    public static void main(String[] args) {
+        int []twoDimArray[] = {{1,2},{1,2},{1,2},{1,2,3,4}};
+        System.out.println(twoDimArray[-1].length);
+        int arr[][] = new int[3][];  
+        arr[0] = new int[3];  
+        arr[1] = new int[4];  
+        arr[2] = new int[2];  // New
+    }
+}
